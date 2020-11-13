244dc58ea1c1c1ead19b22df9eb14e92509040c9
Gracie Renfro <renfrog@iu.edu>
2020-11-13 14:18:05 -0500

    inventory button works

---
+++ Lost in the Woods/Assets/InventoryButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryButton : MonoBehaviour
+{
+    [SerializeField] GameObject panel;
+    [SerializeField] GameObject button;
+    private bool _shown = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonClick() {
+        if (_shown){
+            panel.SetActive(false);
+            _shown = false;
+        }
+        else {
+            panel.SetActive(true);
+            _shown = true;
+        }
+    }
+}
