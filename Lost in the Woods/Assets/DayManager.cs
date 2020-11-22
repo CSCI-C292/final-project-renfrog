e4057208625ce2d649e7b1ccf4e92bb42afcdec0
Gracie Renfro <renfrog@iu.edu>
2020-11-22 13:47:39 -0500

    boundaries and stuff

---
+++ Lost in the Woods/Assets/DayManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayManager : MonoBehaviour
+{
+    float timeLeft = 45.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PassTime();
+    }
+
+    private void PassTime(){
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            timeLeft = 40.0f;
+            Debug.Log( "--another day passes");
+        }
+    }
+}
